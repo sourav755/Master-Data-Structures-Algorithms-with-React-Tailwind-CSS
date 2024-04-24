665945dd6c00d635b6aaf286fb79208ab9a15c02
Sourav <garaisourav143@gmail.com>
2024-04-24 23:46:55 +0530

    mobile responsive done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,18 @@
-function Footer() {}
+import { FaHeart } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="p-10 flex items-center justify-center">
+      Designed with <FaHeart className="mx-1 text-orange-500" /> by
+      <a
+        href="https://github.com/sourav755"
+        target="_blank"
+        className="ml-1 text-blue-600"
+      >
+        Sourav
+      </a>
+    </div>
+  );
+}
 
 export default Footer;
--- src/components/ReviewSection.jsx
+++ src/components/ReviewSection.jsx
@@ -8,25 +8,25 @@ function ReviewSection() {
         "“Walling helps us visually organise ideas and tasks, work collaboratively and efficiently within our team. The visual experience makes it so easy to plan content and stay on track with our projects and campaigns.”",
     },
     {
-      userImage: "./images/Harry.jpg",
-      userName: "Harry Criston",
-      jobRole: "Senior Developer",
+      userImage: "./images/sofia.jpg",
+      userName: "Sofia Senr",
+      jobRole: "Java FullStack Developer",
       review:
-        "“Walling helps us visually organise ideas and tasks, work collaboratively and efficiently within our team. The visual experience makes it so easy to plan content and stay on track with our projects and campaigns.”",
+        "“I've tried several DSA learning platforms, but this one stands out. The interactive challenges make learning fun and engaging. The explanations are clear and concise, and the step-by-step solutions help reinforce my understanding.”",
     },
     {
-      userImage: "./images/Harry.jpg",
-      userName: "Harry Criston",
-      jobRole: "Senior Developer",
+      userImage: "./images/sania.jpg",
+      userName: "Sania D'couste",
+      jobRole: "Software Developer",
       review:
-        "“Walling helps us visually organise ideas and tasks, work collaboratively and efficiently within our team. The visual experience makes it so easy to plan content and stay on track with our projects and campaigns.”",
+        "“I've been using this app to improve my DSA skills, and I'm impressed. The quizzes at the end of each section help me test my knowledge, and the detailed feedback guides me in the right direction. The app is well-designed, with a clean interface that's easy to navigate.”",
     },
     {
-      userImage: "./images/Harry.jpg",
-      userName: "Harry Criston",
-      jobRole: "Senior Developer",
+      userImage: "./images/neeraj.jpg",
+      userName: "Neeraj Agarwal",
+      jobRole: "Senior Developer Associate",
       review:
-        "“Walling helps us visually organise ideas and tasks, work collaboratively and efficiently within our team. The visual experience makes it so easy to plan content and stay on track with our projects and campaigns.”",
+        "“As someone new to DSA, I was looking for an app that could guide me through the basics. This app did not disappoint. The learning paths are well-structured, and the explanations are easy to follow. The built-in code editor allows me to test my code without leaving the app, which is super convenient.”",
     },
   ];
 
@@ -35,18 +35,18 @@ function ReviewSection() {
       {reviews.map((review, index) => (
         <div className="p-4 border-solid border-t-2 border-b-2 m-3">
           <div>
-            <p>{review.review}</p>
+            <p className="italic">{review.review}</p>
           </div>
 
-          <div className="flex text-center">
+          <div className="flex text-center h-[100px]">
             <img
               src={review.userImage}
               alt="userImage"
-              className="h-30 rounded-full"
+              className="h-[70px] w-[70px] object-cover rounded-full items-center m-4"
             />
-            <div className="text-center items-center">
-              <h3>{review.userName}</h3>
-              <h4>{review.jobRole}</h4>
+            <div className="flex flex-col justify-center ml-4">
+              <h3 className="font-playfair font-semibold">{review.userName}</h3>
+              <h4 className="font-lato">{review.jobRole}</h4>
             </div>
           </div>
         </div>
